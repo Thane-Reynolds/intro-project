65129e3442a4c3a54b16c38d0acf698b748bc54c
Thane Reynolds <thane.a.reynolds@gmail.com>
2022-05-08 15:39:54 -0700

    practicing objects in js

---
+++ temp.js
@@ -0,0 +1,35 @@
+const person ={
+    givenName: 'Thane',
+    familyName: 'Reynolds',
+    birthDate: '1990-01-01',
+    birthCity: 'Glendale',    
+    birthCountry: 'United States',
+    hobbies: [
+        {
+            name: 'apex',
+            description: 'APEX Legends, a competitive shooter',
+            frequency: 30,
+        },
+        {
+            name: 'Destiny',
+            description: 'Destiny 2, a looter shooter by small indie developer Bungie',
+            frequency: 10,
+        },
+        {
+            name: 'Reading',
+            description: 'reading fiction or non fiction',
+            frequency: 30,
+        },
+    ],
+    countriesVisited: ['United States'],
+    carsOwned: [
+        {
+            make: 'F.O.R.D.',
+            model: 'Crown Victoria',
+            year: 1997,
+            color: 'red',
+            thingsReplaced: ['catalytic converter', 'alternator', 'tires', 'the whole damn thing'],
+        }
+    ]
+}
+
